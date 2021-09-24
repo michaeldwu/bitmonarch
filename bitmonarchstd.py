cd5ff01b9d73f402349081f2b360f8e3d3e163e7
Eric Sun <ersun1224@gmail.com>
2021-09-24 01:30:47 -0400

    actual rarity sorting

--- bitmonarchstd.py
+++ bitmonarchstd.py
@@ -92,9 +92,7 @@ class BitMonarchStd(Peer):
         # Whenever a peer downloads a piece of the file, it sends per-piece have messages to the peers in its neighborhood. Each peer maintains 
         # an estimate of the availibility of each piece by counting how many of its neighbors have the pieces.
 
-        # NEED TO SORT BY RAREST FIRST RIGHT? Is this the place??
         peers.sort(key=lambda p: p.id)
-
         pieceCount = {}
 
         for peer in peers:
@@ -107,29 +105,19 @@ class BitMonarchStd(Peer):
 
         rarestPieces = sorted(pieceCount, key=pieceCount.get)
 
+        # index is piece ID, value is how often it shows up
+
         # Iterate through all peers
         #    For each peer, request the rarest pieces first
         for peer in peers:
             for piece_id in rarestPieces:
-                if piece_id in np_set and piece_id in peer.available_pieces:
+                if (piece_id in np_set) and (piece_id in peer.available_pieces):
                     start_block = self.pieces[piece_id]
                     r = Request(self.id, peer.id, piece_id, start_block)
                     requests.append(r)
 
-        # Now that we have overall request list
-        # Create new request list that orders requests by rarity
-        request_c = requests.copy()
-        sortedRequests = []
-        for piece_id in rarestPieces:
-            sortedwithinPiece = []
-            for request in request_c:
-                if piece_id == request.piece_id:
-                    sortedwithinPiece.append(request)
-                    request = None
-            random.shuffle(sortedwithinPiece)
-            sortedRequests.extend(sortedwithinPiece)
-
-        return sortedRequests
+        requests = sorted(requests, key=lambda x: (pieceCount[x.piece_id], random.random()))
+        return requests
 
 
     def uploads(self, requests, peers, history):
@@ -159,6 +147,8 @@ class BitMonarchStd(Peer):
         chosen = []
         bws = []
 
+        request_id = list(set([r.requester_id for r in requests]))
+
         if round > 0:
             if round >= 2:
                 i = -2
@@ -167,41 +157,64 @@ class BitMonarchStd(Peer):
             for downloadlist in history.downloads[i:]:
                 # Double counting
                 for download in downloadlist:
-                    if download.from_id in friendliestSet:
-                        friendliestSet[download.from_id] = friendliestSet[download.from_id] + download.blocks
-                    else:
-                        friendliestSet[download.from_id] = download.blocks
+                    # print(download.from_id, request_id)
+                    if download.from_id in request_id:
+                        if download.from_id in friendliestSet:
+                            friendliestSet[download.from_id] = friendliestSet[download.from_id] + download.blocks
+                        else:
+                            friendliestSet[download.from_id] = download.blocks
 
             # Now sort friendlistSet by blocks allowed you to download
             friendliestSet = dict(sorted(friendliestSet.items(), key=lambda item: item[1]))
             # print(friendliestSet)
 
-            if len(friendliestSet.keys()) != 0:
-                chosen = list(friendliestSet.keys())[0:3]
-                if requests is not None and len(requests) != 0:
-                    request_id = list(set([r.requester_id for r in requests]))
-                    for i in chosen:
-                        if i in request_id:
-                            request_id.remove(i)
-                    
-                    # Optimistic Unchoking
-                    if round % 3 == 0:
-                        if len(request_id) != 0:
-                            self.optimisticUnchoked = random.choice(request_id)
-                try:
-                    chosen.append(self.optimisticUnchoked)
-                except:
-                    chosen.append(list(friendliestSet.keys())[4])
+            num_chosen = min(len(friendliestSet), 3)
+            for peer_id in list(friendliestSet.keys())[0:num_chosen]:
+                chosen.append(peer_id)
+
+            if len(requests) != 0:
+                random.shuffle(request_id)
+                for rand_peer in request_id:
+                    if len(chosen) < 4 and  (rand_peer not in chosen):
+                        chosen.append(rand_peer)
+
+            if len(chosen) != 0:
                 bws = even_split(self.up_bw, len(chosen))
-        else:
-            #random from requests
-            if requests:
-                requestsApproved = random.sample(requests, 4)
-                chosen = [requestsApproved[0].requester_id, requestsApproved[1].requester_id, requestsApproved[2].requester_id]
-                bws = even_split(self.up_bw, len(chosen))
+            else:
+                bws = []
+        #
+        #     if len(friendliestSet.keys()) != 0:
+        #         chosen = list(friendliestSet.keys())[0:3]
+        #         if requests is not None and len(requests) != 0:
+        #             request_id = list(set([r.requester_id for r in requests]))
+        #             for i in chosen:
+        #                 if i in request_id:
+        #                     request_id.remove(i)
+        #
+        #             # Optimistic Unchoking
+        #             if round % 3 == 0:
+        #                 if len(request_id) != 0:
+        #                     self.optimisticUnchoked = random.choice(request_id)
+        #         try:
+        #             chosen.append(self.optimisticUnchoked)
+        #         except:
+        #             chosen.append(list(friendliestSet.keys())[4])
+        #         bws = even_split(self.up_bw, len(chosen))
+        #     else:
+        #         if requests:
+        #             requestsApproved = random.sample(requests, min(len(requests), 4))
+        #             chosen = [x.requester_id for x in requestsApproved]
+        #             bws = even_split(self.up_bw, len(chosen))
+        # else:
+        #     #random from requests
+        #     if requests:
+        #
+        #         requestsApproved = random.sample(requests, min(len(requests), 4))
+        #         chosen = [requestsApproved[0].requester_id, requestsApproved[1].requester_id, requestsApproved[2].requester_id]
+        #         bws = even_split(self.up_bw, len(chosen))
 
         # create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
                    for (peer_id, bw) in zip(chosen, bws)]
-
+        print(chosen)
         return uploads
