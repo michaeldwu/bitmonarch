f120a4aa309402101523be6733c2507448035c9c
Eric Sun <ersun1224@gmail.com>
2021-09-23 22:32:07 -0400

    tyrant start

--- bitmonarchtyrant.py
+++ bitmonarchtyrant.py
@@ -18,7 +18,8 @@ class BitMonarchTyrant(Peer):
         print(("post_init(): %s here!" % self.id))
         self.dummy_state = dict()
         self.dummy_state["cake"] = "lie"
-    
+        self.uploadHistory = []
+
     def requests(self, peers, history):
         """
         peers: available info about the peers (who has what pieces)
@@ -32,7 +33,6 @@ class BitMonarchTyrant(Peer):
         needed_pieces = list(filter(needed, list(range(len(self.pieces)))))
         np_set = set(needed_pieces)  # sets support fast intersection ops.
 
-
         logging.debug("%s here: still need pieces %s" % (
             self.id, needed_pieces))
 
@@ -44,67 +44,123 @@ class BitMonarchTyrant(Peer):
         logging.debug("look at the AgentHistory class in history.py for details")
         logging.debug(str(history))
 
-        requests = []   # We'll put all the things we want here
+        requests = []  # We'll put all the things we want here
         # Symmetry breaking is good...
         random.shuffle(needed_pieces)
-        
-        # Sort peers by id.  This is probably not a useful sort, but other 
-        # sorts might be useful
-        peers.sort(key=lambda p: p.id)
-        # request all available pieces from all peers!
-        # (up to self.max_requests from each)
-        for peer in peers:
-            av_set = set(peer.available_pieces)
-            isect = av_set.intersection(np_set)
-            n = min(self.max_requests, len(isect))
-            # More symmetry breaking -- ask for random pieces.
-            # This would be the place to try fancier piece-requesting strategies
-            # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
-                start_block = self.pieces[piece_id]
-                r = Request(self.id, peer.id, piece_id, start_block)
-                requests.append(r)
 
-        return requests
+        # Whenever a peer downloads a piece of the file, it sends per-piece have messages to the peers in its neighborhood. Each peer maintains
+        # an estimate of the availibility of each piece by counting how many of its neighbors have the pieces.
+
+        # NEED TO SORT BY RAREST FIRST RIGHT? Is this the place??
+        peers.sort(key=lambda p: p.id)
+
+        pieceCount = {}
+
+        for peer in peers:
+            avail_pieces = peer.available_pieces
+            for piece in avail_pieces:
+                if piece in pieceCount:
+                    pieceCount[piece] += 1
+                else:
+                    pieceCount[piece] = 1
+
+        rarestPieces = sorted(pieceCount, key=pieceCount.get)
+
+        # Iterate through all peers
+        #    For each peer, request the rarest pieces first
+        for peer in peers:
+            for piece_id in rarestPieces:
+                if piece_id in np_set and piece_id in peer.available_pieces:
+                    start_block = self.pieces[piece_id]
+                    r = Request(self.id, peer.id, piece_id, start_block)
+                    requests.append(r)
+
+        # Now that we have overall request list
+        # Create new request list that orders requests by rarity
+        request_c = requests.copy()
+        sortedRequests = []
+        for piece_id in rarestPieces:
+            sortedwithinPiece = []
+            for request in request_c:
+                if piece_id == request.piece_id:
+                    sortedwithinPiece.append(request)
+                    request = None
+            random.shuffle(sortedwithinPiece)
+            sortedRequests.extend(sortedwithinPiece)
+
+        return sortedRequests
 
     def uploads(self, requests, peers, history):
         """
         requests -- a list of the requests for this peer for this round
-        peers -- available info about all the peers
+        peers -- available info about all the peers. Will contain available histories and
         history -- history for all previous rounds
+
+        example history:
+        AgentHistory(downloads=[[Download(from_id=Seed0, to_id=BitMonarchStd2, piece=0, blocks=1)], [Download(from_id=Seed0, to_id=BitMonarchStd2, piece=1, blocks=1)]], uploads=[[], []])
 
         returns: list of Upload objects.
 
         In each round, this will be called after requests().
+
+
+            * Sort who's friendliest first to you (highest number of blocks in AgentHistory)
+            * Check what pieces they need
+            * If you have the piece they need, put them in your Upload slot
         """
-
         round = history.current_round()
-        logging.debug("%s again.  It's round %d." % (
-            self.id, round))
-        # One could look at other stuff in the history too here.
-        # For example, history.downloads[round-1] (if round != 0, of course)
-        # has a list of Download objects for each Download to this peer in
-        # the previous round.
 
-        if len(requests) == 0:
-            logging.debug("No one wants my pieces!")
-            chosen = []
-            bws = []
+        # Find everyone who's allowed you to download from them before and add you to this set
+        friendliestSet = {}
+
+        # 3 Upload Slots Normally
+        chosen = []
+        bws = []
+
+        if round > 0:
+            if round >= 2:
+                i = -2
+            else:
+                i = -1
+            for downloadlist in history.downloads[i:]:
+                # Double counting
+                for download in downloadlist:
+                    if download.from_id in friendliestSet:
+                        friendliestSet[download.from_id] = friendliestSet[download.from_id] + download.blocks
+                    else:
+                        friendliestSet[download.from_id] = download.blocks
+
+            # Now sort friendlistSet by blocks allowed you to download
+            dict(sorted(friendliestSet.items(), key=lambda item: item[1]))
+            # print(friendliestSet)
+
+            if len(friendliestSet.keys()) != 0:
+                chosen = list(friendliestSet.keys())[0:3]
+                if requests is not None and len(requests) != 0:
+                    request_id = list(set([r.requester_id for r in requests]))
+                    for i in chosen:
+                        if i in request_id:
+                            request_id.remove(i)
+
+                    # Optimistic Unchoking
+                    if round % 3 == 0:
+                        if len(request_id) != 0:
+                            self.optimisticUnchoked = random.choice(request_id)
+                try:
+                    chosen.append(self.optimisticUnchoked)
+                except:
+                    chosen.append(list(friendliestSet.keys())[4])
+                bws = even_split(self.up_bw, len(chosen))
         else:
-            logging.debug("Still here: uploading to a random peer")
-            # change my internal state for no reason
-            self.dummy_state["cake"] = "pie"
-
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
+            # random from requests
+            if requests:
+                requestsApproved = random.sample(requests, 4)
+                chosen = [requestsApproved[0].requester_id, requestsApproved[1].requester_id,
+                          requestsApproved[2].requester_id]
+                bws = even_split(self.up_bw, len(chosen))
 
         # create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
                    for (peer_id, bw) in zip(chosen, bws)]
-            
+
         return uploads
